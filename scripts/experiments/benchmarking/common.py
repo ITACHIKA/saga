3519cbca8fc13df60d4d957f7029dd844562d86b
ITACH1KA <iamwojiaotm@outlook.com>
2026-07-26 18:02:29 -0700

    save local saga

--- scripts/experiments/benchmarking/common.py
+++ scripts/experiments/benchmarking/common.py
@@ -62,11 +62,11 @@ exclude_datasets: Set[str] = {
     "srasearch",
     "blast",
     # exclude the following for testing purpose since cppHeft and Heft produce identical result
-    "etl",
-    "in_trees",
-    "out_trees",
-    "predict",
-    "stats"
+    # "etl",
+    # "in_trees",
+    # "out_trees",
+    # "predict",
+    # "stats"
 }
 
 datadir = thisdir.joinpath("data", "benchmarking")
